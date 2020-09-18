4c485601c6027190dd4fef1f349f0b718cd0087c
Mehdi Amirsoltani <behrad@ausecure.com>
2020-09-17 22:54:16 -0400

    Revert "updating test script"
    This reverts commit 6a6c93619072f6adec31be582a78beb0e95145ed.

--- .github/workflows/test_app.sh
+++ .github/workflows/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace Runner.xcworkspace \
-            -scheme Runner\ iOS \
+xcodebuild -workspace Calculator.xcworkspace \
+            -scheme Calculator\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
